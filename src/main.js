b521a36bfa138d83cc057f6eb547adb00d5f734c
ShahSolutionX <asrol@solutionx.com.my>
2023-01-10 17:52:11 +0800

    updated

--- src/main.js
+++ src/main.js
@@ -1,4 +1,18 @@
-const { createApp } = require('vue');
-import App from "./App.vue";
+import Vue from 'vue'
+import App from './App.vue'
+import ElementUI from 'element-ui';
+import router from './route/route.js';
+import VuePageTransition from 'vue-page-transition'
+import VueYoutube from 'vue-youtube'
+import './styles/UI-theme/index.css'
 
-createApp(App).mount("#app");
+Vue.config.productionTip = false;
+
+Vue.use(ElementUI);
+Vue.use(VuePageTransition);
+Vue.use(VueYoutube);
+
+new Vue({
+    render: h => h(App),
+    router
+}).$mount('#app');
